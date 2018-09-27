3b871d7f3bd44441ac48756b09c0a8d5e1f03c73
Jose Ignacio Retamal <peperetamalthomsen@outlook.com>
2018-09-27 22:07:49 +0100

    find first...

--- FirstUniqueCharacterString.java
+++ FirstUniqueCharacterString.java
@@ -2,13 +2,10 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 
 /*
- *	Jose Ignacio Retamal Thomsen: Interview Question.
  *	
  *	Question: 
  * 		Find the first non-repeated character in a String
  * 
- * 	First Approach:
- * 		-No case sensitive (a=A).
  *		
  * 
  */
@@ -20,20 +17,16 @@ public class FirstUniqueCharacterString
 		System.out.println(
 				"s-" + firstUniqueCharA1("ssmsmuugfghfhfffsvdsatgregfsfxnfgbdxfsfghhtgfgxztytyhgfbfbdtgggrtyhfgvae"));
 
+		
+
 		System.out.println(
-				"s-" + firstUniqueCharA2("ssmsmuugfghfhfffsvdsatgregfsfxnfgbdxfsfghhtgfgxztytyhgfbfbdtgggrtyhfgvae"));
+				"s-" + firstUniqueCharA2("ssmsmuugfghfhfffsvdsatgregfsfxfgbdxfsfghhtgfgxztytyhgfbfbdtgggrtyhfgvaeN"));
 
 		System.out.println(
 				"s-" + firstUniqueCharA3("ssmsmuugfghfhfffsvdsatgregfsfxfgbdxfsfghhtgfgxztytyhgfbfbdtgggrtyhfgvaeN"));
+		System.out.println("s-" + firstUniqueCharA3("sN"));
 
-		System.out.println(
-				"s-" + firstUniqueCharA4("ssmsmuugfghfhfffsvdsatgregfsfxfgbdxfsfghhtgfgxztytyhgfbfbdtgggrtyhfgvaeN"));
-		System.out.println("s-" + firstUniqueCharA4("sN"));
-
-		Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
-		System.out.println("x" + ht.put('a', 1));
-		System.out.println(ht.put('a', 3));
-		System.out.println(ht.get('a'));
+		
 
 	}
 
@@ -77,52 +70,13 @@ public class FirstUniqueCharacterString
 		return 0;
 	}
 
+
+
 	/*
-	 * Using Hashtable and array
-	 * 
-	 * 1)Count how many appearance of each character. 2) create a hashtable with all
-	 * unique character and the position of then 3) find the character with smaller
-	 * position in the hash table
+	 * Jose Retamal 
+	 * Counting array
 	 */
 	public static char firstUniqueCharA2(String strP)
-	{
-		Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
-
-		int a[] = new int[26];
-
-		String str = strP.toUpperCase();
-
-		for (int i = 0; i < str.length(); i++)
-		{
-			if (a[str.charAt(i) - 'A'] == 0)
-			{
-				ht.put(str.charAt(i), i);
-				a[str.charAt(i) - 'A']++;
-			} else
-			{
-				a[str.charAt(i) - 'A']++;
-				ht.remove(str.charAt(i));
-			}
-
-		}
-
-		char firstUnique = 0;
-
-		int firstIndex = str.length();
-
-		for (char c : ht.keySet())
-		{
-			if (ht.get(c) < firstIndex)
-			{
-				firstIndex = ht.get(c);
-				firstUnique = c;
-			}
-		}
-
-		return firstUnique;
-	}
-
-	public static char firstUniqueCharA3(String strP)
 	{
 		int a[] = new int[26];
 
@@ -160,7 +114,12 @@ public class FirstUniqueCharacterString
 
 	}
 
-	public static char firstUniqueCharA4(String strP)
+	/*
+	 * Jose Retamal 
+	 * HashTable
+	 */
+	
+	public static char firstUniqueCharA3(String strP)
 	{
 		Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
 
